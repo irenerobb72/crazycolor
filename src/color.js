c2c77194cbaacb256957531b46ab0b1bd98d45e7
Irene Robb <irenerobb72@gmail.com>
2016-05-05 13:35:08 -0400

    getrnd color function

--- src/color.js
+++ src/color.js
@@ -15,3 +15,10 @@ for(; y < dia; x++){
 ctx.globalCompositeOperation = 'destination-in'
 ctx.arc(radius, radius, 0, 2*Math.PI)
 ctx.fill()
+
+function getRndColor() {
+  var r = 255*Math.random()|0,
+      g = 255*Math.random()|0,
+      b = 255*Math.random()|0;
+  return 'rgb(' + r + ',' + g + ',' + b + ')'
+}
